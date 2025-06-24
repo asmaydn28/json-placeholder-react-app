712f0e536ffb629c34243efb94fb3c36eea54b7f
asmaydn28 <asmaydn2828@gmail.com>
2025-06-24 14:43:58 +0300

    redirects

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import router from './Routes.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+//TASARIM İÇİN CURSOR KULLANILDI.
 createRoot(document.getElementById('root')!).render(
   <RouterProvider router={router}/>
 );
